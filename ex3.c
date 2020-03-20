6a7b6e19662fbb03fb25918e8d3dc9d0770bde4b
Ander-creator <61889841+Ander-creator@users.noreply.github.com>
2020-03-19 21:38:35 -0300

    Update ex3.c
    Ainda não consegui implementar uma função que conte os níveis da árvore.

--- ex3.c
+++ ex3.c
@@ -2,73 +2,77 @@
 #include<stdio.h>
 #define Tam_max 5
 	
-	typedef struct{
+	typedef struct empregado{
 		int emp;
-		int subordinados[Tam_max];
+		struct empregado*  subordinados[Tam_max];
 		int n_sub;
 	} empregado;
-	
+
 	empregado novo(int num){
 		empregado e;
 		e.emp = num;
 		e.n_sub = 0;
+		int i = 0;
+		for (i; i<Tam_max; i++) e.subordinados[i]=NULL;
 		return e;
 	}
 	
 	void adicionar_empregado(empregado *atual, empregado *subalterno){
 		if(atual->n_sub < 5){
-			(atual->subordinados[(atual->n_sub)]) = (subalterno->emp);
+			atual->subordinados[(atual->n_sub)] = subalterno;
 			(atual->n_sub)++;
 		}
 	}
-	
+	//Função que conta os empregados direto de cada nó
 	void contar_empregados(empregado e){
-		printf("O funcionário %d tem %d subalternos\n", e.emp, e.n_sub);	
+		printf("O funcionário %d tem %d subalternos diretos\n", e.emp, e.n_sub);	
 	}
-	/*void printar(empregado e){
-		printf("empregado: %d \n", e.emp);
+	
+	//vamos implementar a função recursiva que acessa todos os nós
+	int todos_empregados(empregado *pai){
+		int i = 0, valor = 0;
+		while( (i<Tam_max) && ((pai->subordinados[i]) != NULL) ){
+			valor += todos_empregados((pai->subordinados[i]));
+			i++;
+		}
+		return valor += pai->n_sub;
+	}
+	
+	/*tentativa de implentar o contador de níveis, fora isso tudo funciona.
+	
+	 *int contar_niveis(){
+		int i = 0, niveis = 0;
+		while( (i<Tam_max) && ((pai->subordinados[i]) != NULL) ){
+			contar_niveis((pai->subordinados[i]));
+			i++;
+		}
+		
+		return niveis;
 	}*/
-
-
-
+	
 	int main(){
-		empregado e1, e2, e3, e4, e5, e6, e7, e8, e9;
-		
-		e1 = novo(1);
-		e2 = novo(2);
-		e3 = novo(3);
-		e4 = novo(4);
-		e5 = novo(5);
-		e6 = novo(6);	
-		e7 = novo(7);
-		e8 = novo(8);
-		e9 = novo(9);
-		
+		empregado e[9];
+		int i;
+		for(i = 0; i<9; i++) e[i] = novo(i);
+	
 		//empregados primeiro nível
-		adicionar_empregado(&e9, &e1);
-		adicionar_empregado(&e9, &e6);
-		adicionar_empregado(&e9, &e3);
-		adicionar_empregado(&e9, &e8);	
+		adicionar_empregado(&e[8], &e[0]);
+		adicionar_empregado(&e[8], &e[5]);
+		adicionar_empregado(&e[8], &e[2]);
+		adicionar_empregado(&e[8], &e[7]);	
 		//empregados segundo nível
-		adicionar_empregado(&e1, &e2);
-		adicionar_empregado(&e1, &e4);
-		adicionar_empregado(&e3, &e5);
-		adicionar_empregado(&e8, &e7);
-		
-		contar_empregados(e1);
-		contar_empregados(e2);
-		contar_empregados(e3);
-		contar_empregados(e4);
-		contar_empregados(e5);
-		contar_empregados(e6);
-		contar_empregados(e7);
-		contar_empregados(e8);
-		contar_empregados(e9);
-				
-//		printar(e1);
-//		printar(e2);
-//		printar(e3);
+		adicionar_empregado(&e[0], &e[1]);
+		adicionar_empregado(&e[0], &e[3]);
+		adicionar_empregado(&e[2], &e[4]);
+		adicionar_empregado(&e[7], &e[6]);
+		for(i=0; i<9; i++)contar_empregados(e[i]);
+		int j = 0;
+		for(j; j<9; j++){
+		int total = 0 ;
+		total = todos_empregados(&e[j]);
+		printf("o empregado %d tem %d subordinados totais\n", e[j].emp, total);
+		}
+
 		return 0;
 	}
-
 
